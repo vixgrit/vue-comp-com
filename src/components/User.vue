121d1398b538493ea0c78e54330329e636fef8c6
Jonathan Kvicky <root.vixgrit@gmail.com>
2017-06-03 18:11:05 -0400

    use props for parent-to-child communication

--- src/components/User.vue
+++ src/components/User.vue
@@ -1,29 +1,40 @@
 <template>
-    <div class="component">
-        <h1>The User Component</h1>
-        <p>I'm an awesome User!</p>
-        <hr>
-        <div class="row">
-            <div class="col-xs-12 col-sm-6">
-                <app-user-detail></app-user-detail>
-            </div>
-            <div class="col-xs-12 col-sm-6">
-                <app-user-edit></app-user-edit>
-            </div>
-        </div>
+  <div class="component">
+    <h1>The User Component</h1>
+    <p>I'm an awesome User!</p>
+    <button @click="changeName">Change my Name</button>
+    <hr>
+    <div class="row">
+      <div class="col-xs-12 col-sm-6">
+        <app-user-detail :name="name"></app-user-detail>
+      </div>
+      <div class="col-xs-12 col-sm-6">
+        <app-user-edit></app-user-edit>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-    import UserDetail from './UserDetail.vue';
-    import UserEdit from './UserEdit.vue';
+  import UserDetail from './UserDetail.vue';
+  import UserEdit from './UserEdit.vue';
 
-    export default {
-        components: {
-            appUserDetail: UserDetail,
-            appUserEdit: UserEdit
-        }
+  export default {
+    data: function() {
+      return {
+        name: 'Max'
+      };
+    },
+    methods: {
+      changeName() {
+        this.name = 'Anna';
+      }
+    },
+    components: {
+      appUserDetail: UserDetail,
+      appUserEdit: UserEdit
     }
+  }
 </script>
 
 <style scoped>
