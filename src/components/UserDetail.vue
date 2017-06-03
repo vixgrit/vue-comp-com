f7d1e8a25e5d6e20e6595cb32daebbbad129eff1
Jonathan Kvicky <root.vixgrit@gmail.com>
2017-06-03 18:32:32 -0400

    example of validating props

--- src/components/UserDetail.vue
+++ src/components/UserDetail.vue
@@ -2,13 +2,23 @@
   <div class="component">
     <h3>You may view the User Details here</h3>
     <p>Many Details</p>
-    <p>User name: {{ name }}</p>
+    <p>User name: {{ switchName() }}</p>
   </div>
 </template>
 
 <script>
   export default {
-    props: ['name']
+    props: {
+      myName: {
+        type: String,
+        default: 'Max'
+      }
+    },
+    methods: {
+      switchName() {
+        return this.myName.split('').reverse().join('');
+      }
+    }
   }
 </script>
 
