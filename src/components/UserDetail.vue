aae67d7bd9180227585170153da309afdee9cbae
Jonathan Kvicky <root.vixgrit@gmail.com>
2017-06-03 19:03:47 -0400

    use custom events for child-to-parent communication

--- src/components/UserDetail.vue
+++ src/components/UserDetail.vue
@@ -3,6 +3,7 @@
     <h3>You may view the User Details here</h3>
     <p>Many Details</p>
     <p>User name: {{ switchName() }}</p>
+    <button @click="resetName">Reset Name</button>
   </div>
 </template>
 
@@ -17,6 +18,10 @@
     methods: {
       switchName() {
         return this.myName.split('').reverse().join('');
+      },
+      resetName() {
+        this.myName = 'Max';
+        this.$emit('nameWasReset', this.myName);
       }
     }
   }
